2fb44b9d49605df88e220627f22c043adc162d01
Sharon Garcia <sfloresgarcia@uade.edu.ar>
2026-05-18 21:36:20 -0300

    Correcion del submarino

--- Submarino/Submarino.java
+++ Submarino/Submarino.java
@@ -20,30 +20,92 @@ Mantengamos una comunicación fluida para evitar conflictos en la integración d
 
 */
 
-package submarino;
+    package submarino;
 
 public class Submarino {
-    
+
     private float posX;
     private float posY;
     private float vida;
     private int vidas;
     private int puntaje;
+    // Constantes para el submarino
+    private static final float VIDA_MAXIMA = 100.0f;
+    private static final int VIDAS_INICIALES = 5;
+    private static final int PUNTAJE_INICIAL = 0;
+    private int puntosParaSiguienteVidaExtra;
+    // Límites de movimiento del submarino
+    private static final float LIMITE_SUPERIOR_Y = 300.0f;
+    private static final float LIMITE_INFERIOR_Y = 800.0f;
+    // Objetivo de puntos para ganar una vida extra
+    private static final int OBJETIVO_PUNTOS_VIDA_EXTRA = 500;
 
-    public void mover(float dx, float dy, int anchoPantalla)
-    {
 
-    };
+    public Submarino(float posX, float posY) {
+        //Hago esto por que el submarino tiene un rango de movimiento limitado
+        if (posY >= LIMITE_SUPERIOR_Y && posY <= LIMITE_INFERIOR_Y) {
+            this.posY = posY;
+        } else {
+            this.posY = LIMITE_SUPERIOR_Y; // Valor por defecto seguro
+        }
+        this.posX = posX;
+        this.vida = VIDA_MAXIMA;
+        this.vidas = VIDAS_INICIALES;
+        this.puntaje = PUNTAJE_INICIAL;
+        this.puntosParaSiguienteVidaExtra = OBJETIVO_PUNTOS_VIDA_EXTRA;
+    }
+    public void moverX(float dx) {
+        posX += dx;
+    }
+
+    public void moverY(float dy) {
+
+        float nuevaPosY = posY + dy;
+        //verifico que se cumpla los limites
+        if (nuevaPosY >= LIMITE_SUPERIOR_Y && nuevaPosY <= LIMITE_INFERIOR_Y) {
+            posY = nuevaPosY;
+        }
+    }
+
+
+
+
     public void recibirDanio(float cantidad)
     {
+        vida -= cantidad;
+        if (vida <= 0) {
+            perderVida();
+        }
 
     };
+
+    public void perderVida()
+    {
+        vidas--;
+        if (vidas > 0) {
+            vida = VIDA_MAXIMA; // Reinicio la vida al perder una de las vidas
+        } else {
+            System.out.println("GAME OVER");
+        }
+    };
+
+
     public void sumarPuntos(int puntos)
     {
-
+        puntaje+=puntos;
+        if (puntaje >= puntosParaSiguienteVidaExtra) {
+            evaluarVidaExtra();
+        }
     };
     public void evaluarVidaExtra()
     {
+        if (puntaje >= puntosParaSiguienteVidaExtra) {
+            vidas++;
+            //Guardo el nuevo valor de los puntos necesarios para la próxima vida extra
+            puntosParaSiguienteVidaExtra += OBJETIVO_PUNTOS_VIDA_EXTRA; // Incremento el objetivo para la próxima vida extra
+            System.out.println("¡Vida extra obtenida!");
+
+        }
 
     };
 
