110cdb5cee8c08312fa0d5a438bee7b01e3d4efa
Sharon Garcia <sfloresgarcia@uade.edu.ar>
2026-05-19 22:12:38 -0300

    Metodos de barco

--- src/barco/Barco.java
+++ src/barco/Barco.java
@@ -24,9 +24,11 @@ Mantengamos una comunicación fluida para evitar conflictos en la integración d
 
 package barco;
 
-import java.util.List;
+
 
 import carga.Carga;
+import java.util.Random;
+
 
 public class Barco {
     
@@ -34,32 +36,53 @@ public class Barco {
     private float posY;
     private float velocidad;
     private int direccion;
-    private float ultimaPosicionLanzamiento;
-    private List<Carga> cargasDisponibles;  
+    private Random random;
     
-    public Barco(float velocidad)
+    public Barco(float velocidad, int anchoPantalla)
     {
     	this.velocidad=velocidad;
+        this.posY = 0.0f; 
+        this.random = new Random();
+        if (random.nextBoolean()) {
+            this.posX = 0.0f;
+            this.direccion = 1; // Se mueve hacia la derecha (+)
+        } else {
+            this.posX = anchoPantalla;
+            this.direccion = -1; // Se mueve hacia la izquierda (-)
+        }
+       
+    
     }
 
     public void avanzar()
     {
+        posX += velocidad * direccion;
+
 
     };
+
     public boolean verificarLimites(int anchoPantalla)
     {
-    	return true;
-    };
-    public boolean verificarLanzamientoCarga()
-    {
-    	return true;
-    };
-    public void agregarCarga(Carga carga)
-    {
+        //Verifico si la posicion esta dentro de los parametros.
+        if (posX < 0 || posX > anchoPantalla) {
+            return false; // El barco ha salido de los límites
+        }
 
+        return true;
     };
+    public boolean verificarLanzamientoCarga(int anchoPantalla)
+    {
+        if(verificarLimites(anchoPantalla)) {
+
+        //Utilizo random para que haya una probabilidad del 5% de lanzar una carga.
+        return random.nextInt(100) < 5;
+    }
+    	return  false;       
+    };
+   
     public Carga soltarCarga()
     {
+
     	return new Carga(velocidad);
     };
     
@@ -67,6 +90,18 @@ public class Barco {
     {
     	return this.posX;
     }
-
-
+    public float getPosY()
+    {
+    	return this.posY;
+    }
+    public float getVelocidad()
+    {
+    	return this.velocidad;
+        
+    }
+    public int getDireccion()
+    {
+    	return this.direccion;
+    }
+   
 }
